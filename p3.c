03d6d2e3cf04e8ffb2a68b3961e9756b6c6f9329
abhishekawaradi <abhishekawaradi01@gmail.com>
2026-03-23 18:19:09 +0530

    Fix formatting and remove unnecessary whitespace

--- p3.c
+++ p3.c
@@ -1,35 +1,27 @@
 #include<stdio.h>
 #define INF 999
-
 int min(int a,int b)
 {
     return (a<b)?a:b;
 }
-
 void floyd(int p[10][10],int n)
 {
     int i,j,k;
-
     for(k=1;k<=n;k++)
         for(i=1;i<=n;i++)
             for(j=1;j<=n;j++)
                 p[i][j]=min(p[i][j],p[i][k]+p[k][j]);
 }
-
 int main()
 {
     int a[10][10],n,i,j;
-
     printf("Enter n value: ");
     scanf("%d",&n);
-
     printf("Enter graph data:\n");
     for(i=1;i<=n;i++)
         for(j=1;j<=n;j++)
             scanf("%d",&a[i][j]);
-
     floyd(a,n);
-
     printf("Shortest path matrix:\n");
     for(i=1;i<=n;i++)
     {
@@ -37,6 +29,5 @@ int main()
             printf("%d ",a[i][j]);
         printf("\n");
     }
-
     return 0;
 }
