c35d1f2359bb54fc4cbd0cbe8c5cc521e31f691f
abhishekawaradi <abhishekawaradi01@gmail.com>
2026-04-13 07:06:32 +0530

    Update p5.c

--- p5.c
+++ p5.c
@@ -1,41 +1,31 @@
 #include<stdio.h>
-
 int temp[10],k=0;
-
 void sort(int a[10][10],int id[10],int n)
 {
     int i,j;
-
     for(i=1;i<=n;i++)
     {
         if(id[i]==0)
         {
             id[i]=-1;
             temp[++k]=i;
-
             for(j=1;j<=n;j++)
             {
                 if(a[i][j]==1 && id[j]!=-1)
                     id[j]--;
             }
-
             i=0;
         }
     }
 }
-
 int main()
 {
     int a[10][10],id[10],n,i,j;
-
     printf("Enter n value: ");
     scanf("%d",&n);
-
     for(i=1;i<=n;i++)
         id[i]=0;
-
     printf("Enter graph data:\n");
-
     for(i=1;i<=n;i++)
         for(j=1;j<=n;j++)
         {
@@ -43,9 +33,7 @@ int main()
             if(a[i][j]==1)
                 id[j]++;
         }
-
     sort(a,id,n);
-
     if(k!=n)
         printf("Topological ordering not possible");
     else
@@ -54,6 +42,5 @@ int main()
         for(i=1;i<=k;i++)
             printf("%d ",temp[i]);
     }
-
     return 0;
 }
