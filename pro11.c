abb07944e147e612f4b72245dc0f85bf77c8dd7c
abhishekawaradi <abhishekawaradi01@gmail.com>
2026-07-12 15:50:56 +0530

    Update pro11.c

--- pro11.c
+++ pro11.c
@@ -2,68 +2,15 @@
 #include<stdlib.h>
 #include<sys/time.h>
 #include<time.h>
-void fnGenRandInput(int [], int);
-void fnDispArray(int [], int);
-void fnMerge(int [], int ,int ,int);
-void fnMergeSort(int [], int , int);
-int main()
+void randominput(int a[],int n)
 {
-    FILE *fp,*gp;
-    struct timeval tv;
-    double start,end;
-    int a[500000],n,i,choice;
-    for(;;)
-    {
-        printf("\n1.Plot Graph\n2.MergeSort\n3.Exit");
-        printf("\nEnter choice: ");
-        scanf("%d",&choice);
-        switch(choice)
-        {
-            case 1:
-                fp=fopen("MergePlot.dat","w");
-                for(i=100;i<100000;i+=100)
-                {
-                    fnGenRandInput(a,i);
-                    gettimeofday(&tv,NULL);
-                    start=tv.tv_sec+(tv.tv_usec/1000000.0);
-                    fnMergeSort(a,0,i-1);
-                    gettimeofday(&tv,NULL);
-                    end=tv.tv_sec+(tv.tv_usec/1000000.0);
-                    fprintf(fp,"%d\t%lf\n",i,end-start);
-                }
-                fclose(fp);
-                gp=fopen("MergePlot.gpl","w");
-                fprintf(gp,"set terminal png\n");
-                fprintf(gp,"set title 'Time Complexity for Merge Sort'\n");
-                fprintf(gp,"set xlabel 'Input Size'\n");
-                fprintf(gp,"set ylabel 'Time Taken'\n");
-                fprintf(gp,"set grid\n");
-                fprintf(gp,"set output 'MergePlot.png'\n");
-                fprintf(gp,"plot 'MergePlot.dat' title 'Merge Sort' with lines\n");
-                fclose(gp);
-                printf("\nMergePlot.dat and MergePlot.gpl created\n");
-                printf("Run: gnuplot MergePlot.gpl\n");
-                break;
-            case 2:
-                printf("\nEnter number of elements: ");
-                scanf("%d",&n);
-                fnGenRandInput(a,n);
-                printf("\nUnsorted Array\n");
-                fnDispArray(a,n);
-                fnMergeSort(a,0,n-1);
-                printf("\nSorted Array\n");
-                fnDispArray(a,n);
-                break;
-            case 3:
-                exit(0);
-        }
-    }
+    int i;
+    for(i=0;i<n;i++)
+        a[i]=rand()%10000;
 }
-void fnMerge(int a[], int low,int mid,int high)
+void merge(int a[],int low,int mid,int high)
 {
-    int i,j,k,b[500000];
-    i=k=low;
-    j=mid+1;
+    int i=low,j=mid+1,k=low,b[100000];
     while(i<=mid && j<=high)
     {
         if(a[i]<a[j])
@@ -75,30 +22,60 @@ void fnMerge(int a[], int low,int mid,int high)
         b[k++]=a[i++];
     while(j<=high)
         b[k++]=a[j++];
-    for(i=low;i<k;i++)
+    for(i=low;i<=high;i++)
         a[i]=b[i];
 }
-void fnMergeSort(int a[],int low,int high)
+void mergesort(int a[],int low,int high)
 {
     int mid;
     if(low<high)
     {
         mid=(low+high)/2;
-        fnMergeSort(a,low,mid);
-        fnMergeSort(a,mid+1,high);
-        fnMerge(a,low,mid,high);
+        mergesort(a,low,mid);
+        mergesort(a,mid+1,high);
+        merge(a,low,mid,high);
     }
 }
-void fnGenRandInput(int X[], int n)
+int main()
 {
-    int i;
+    int a[100000],size[20];
+    int t,n,i;
+    struct timeval tv;
+    double start,end;
     srand(time(NULL));
-    for(i=0;i<n;i++)
-        X[i]=rand()%10000;
+    printf("Enter number of test cases: ");
+    scanf("%d",&t);
+    for(i=0;i<t;i++)
+    {
+        do
+        {
+            printf("Enter number of elements %d (>5000): ",i+1);
+            scanf("%d",&size[i]);
+            if(size[i]<=5000)
+                printf("Please enter n greater than 5000.\n");
+        }while(size[i]<=5000);
+    }
+    printf(" No. of Elements\tTime Taken(sec)\n");
+    for(i=0;i<t;i++)
+    {
+        n=size[i];
+        randominput(a,n);
+        gettimeofday(&tv,NULL);
+        start=tv.tv_sec+tv.tv_usec/1000000.0;
+        mergesort(a,0,n-1);
+        gettimeofday(&tv,NULL);
+        end=tv.tv_sec+tv.tv_usec/1000000.0;
+        printf("%10d\t\t%.6lf\n",n,end-start);
+    }
+    return 0;
 }
-void fnDispArray(int X[], int n)
-{
-    int i;
-    for(i=0;i<n;i++)
-        printf("%d\n",X[i]);
-}
+
+import matplotlib.pyplot as plt
+n_values = [6000,7000,8000,9000,10000]
+time_taken = [0.001352,0.001571,0.001809,0.002036,0.002261]
+plt.plot(n_values, time_taken, marker='o')
+plt.title("Merge Sort Time Complexity")
+plt.xlabel("Number of Elements (n)")
+plt.ylabel("Time Taken (seconds)")
+plt.grid(True)
+plt.show()
