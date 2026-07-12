905469a0393309b7764e3a199c2e726cbac277cb
abhishekawaradi <abhishekawaradi01@gmail.com>
2026-07-12 15:46:22 +0530

    Update pro10.c

--- pro10.c
+++ pro10.c
@@ -2,106 +2,80 @@
 #include<stdlib.h>
 #include<sys/time.h>
 #include<time.h>
-void fnGenRandInput(int [], int);
-void fnDispArray(int [], int);
-int fnPartition(int [], int , int );
-void fnQuickSort(int [], int , int );
-void fnSwap(int*, int*);
-void fnSwap(int *a, int *b)
+void randominput(int a[],int n)
 {
-    int t=*a;
-    *a=*b;
-    *b=t;
+    int i;
+    for(i=0;i<n;i++)
+        a[i]=rand()%10000;
+}
+void quicksort(int a[],int low,int high)
+{
+    int i,j,pivot,temp;
+    if(low<high)
+    {
+        pivot=low;
+        i=low;
+        j=high;
+        while(i<j)
+        {
+            while(a[i]<=a[pivot] && i<high)
+                i++;
+            while(a[j]>a[pivot])
+                j--;
+            if(i<j)
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+        temp=a[pivot];
+        a[pivot]=a[j];
+        a[j]=temp;
+        quicksort(a,low,j-1);
+        quicksort(a,j+1,high);
+    }
 }
 int main()
 {
-    FILE *fp,*gp;
+    int a[100000],size[20];
+    int t,n,i;
     struct timeval tv;
     double start,end;
-    int a[500000],n,i,choice;
-    for(;;)
-    {
-        printf("\n1.Plot Graph\n2.QuickSort\n3.Exit");
-        printf("\nEnter choice: ");
-        scanf("%d",&choice);
-        switch(choice)
-        {
-            case 1:
-                fp=fopen("QuickPlot.dat","w");
-                for(i=100;i<100000;i+=100)
-                {
-                    fnGenRandInput(a,i);
-                    gettimeofday(&tv,NULL);
-                    start=tv.tv_sec+(tv.tv_usec/1000000.0);
-                    fnQuickSort(a,0,i-1);
-                    gettimeofday(&tv,NULL);
-                    end=tv.tv_sec+(tv.tv_usec/1000000.0);
-                    fprintf(fp,"%d\t%lf\n",i,end-start);
-                }
-                fclose(fp);
-                gp=fopen("Quick.gpl","w");
-                fprintf(gp,"set terminal png\n");
-                fprintf(gp,"set title 'Time Complexity for Quick Sort'\n");
-                fprintf(gp,"set xlabel 'Input Size'\n");
-                fprintf(gp,"set ylabel 'Time Taken'\n");
-                fprintf(gp,"set grid\n");
-                fprintf(gp,"set output 'QuickPlot.png'\n");
-                fprintf(gp,"plot 'QuickPlot.dat' title 'Quick Sort' with lines\n");
-                fclose(gp);
-                printf("\nQuickPlot.dat and Quick.gpl created\n");
-                printf("Run: gnuplot Quick.gpl\n");
-                break;
-            case 2:
-                printf("\nEnter number of elements: ");
-                scanf("%d",&n);
-                fnGenRandInput(a,n);
-                printf("\nUnsorted Array\n");
-                fnDispArray(a,n);
-                fnQuickSort(a,0,n-1);
-                printf("\nSorted Array\n");
-                fnDispArray(a,n);
-                break;
-            case 3:
-                exit(0);
-        }
-    }
-}
-int fnPartition(int a[], int l, int r)
-{
-    int i,j,p;
-    p=a[l];
-    i=l;
-    j=r+1;
-    do
-    {
-        do{i++;}while(a[i]<p);
-        do{j--;}while(a[j]>p);
-        fnSwap(&a[i],&a[j]);
-    }while(i<j);
-    fnSwap(&a[i],&a[j]);
-    fnSwap(&a[l],&a[j]);
-    return j;
-}
-void fnQuickSort(int a[], int l, int r)
-{
-    int s;
-    if(l<r)
-    {
-        s=fnPartition(a,l,r);
-        fnQuickSort(a,l,s-1);
-        fnQuickSort(a,s+1,r);
-    }
-}
-void fnGenRandInput(int X[], int n)
-{
-    int i;
     srand(time(NULL));
-    for(i=0;i<n;i++)
-        X[i]=rand()%10000;
+    printf("Enter number of test cases: ");
+    scanf("%d",&t);
+    for(i=0;i<t;i++)
+    {
+        do
+        {
+            printf("Enter number of elements %d (>5000): ",i+1);
+            scanf("%d",&size[i]);
+            if(size[i]<=5000)
+                printf("Please enter n greater than 5000.\n");
+        }while(size[i]<=5000);
+    }
+    printf(" No. of Elements\tTime Taken(sec)\n");
+    for(i=0;i<t;i++)
+    {
+        n=size[i];
+        randominput(a,n);
+        gettimeofday(&tv,NULL);
+        start=tv.tv_sec+tv.tv_usec/1000000.0;
+        quicksort(a,0,n-1);
+        gettimeofday(&tv,NULL);
+        end=tv.tv_sec+tv.tv_usec/1000000.0;
+        printf("%10d\t\t%.6lf\n",n,end-start);
+    }
+    return 0;
 }
-void fnDispArray(int X[], int n)
-{
-    int i;
-    for(i=0;i<n;i++)
-        printf("%d\n",X[i]);
-}
+
+import matplotlib.pyplot as plt
+n_values = [6000,7000,8000,9000,10000]
+time_taken = [0.001821,0.002154,0.002496,0.002861,0.003274]
+plt.plot(n_values, time_taken, marker='o')
+plt.title("Quick Sort Time Complexity")
+plt.xlabel("Number of Elements (n)")
+plt.ylabel("Time Taken (seconds)")
+plt.grid(True)
+plt.show()
